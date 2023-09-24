ef18b942913ce74e3a757c44557cbde99b2dbd16
Xiuqi Feng <106839860+Xiuqifeng@users.noreply.github.com>
2023-09-23 18:52:38 -0700

    pushed code

---
+++ Value Market/value_market/products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+
