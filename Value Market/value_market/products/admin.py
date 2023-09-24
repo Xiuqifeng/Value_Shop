d56e7cbf5cc830214be9b3d5504158dd0d70b033
Xiuqi Feng <106839860+Xiuqifeng@users.noreply.github.com>
2023-09-23 20:52:30 -0700

    Delete Value Market/value_market directory

--- Value Market/value_market/products/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
-
-
-admin.site.register(Product, ProductAdmin)
